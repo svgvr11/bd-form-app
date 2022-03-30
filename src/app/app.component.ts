446b9d1d11f6bab2983e8e983e82ffe1a85261e2
Victor Muñoz Fernandez <vmunozfe@emeal.nttdata.com>
2022-03-30 03:11:50 -0300

    add new component tab done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bd-form-app';
+  title = 'AdderComp v0.1';
 }
--- src/app/components/home/new-component/new-component.component.ts
+++ src/app/components/home/new-component/new-component.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponentComponent implements OnInit {
 
+  exampleCompTitle: string = 'Ej: Monitor Asus VP228HE 21.5" negro';
+  exampleCompProdName: string = 'Ej: VP228HE';
+  exampleCompCod: string = 'Ej: 90LM01K0-B04110';
+  exampleBrand: string = 'Ej: Asus';
+  examplePower: string = 'Ej: 13';
+  examplesCompTypes: any = [
+    { prod: 'Aire acondicionado' },
+    { prod: 'Computación' },
+    { prod: 'Electrodomésticos hogar' },
+    { prod: 'Electro movilidad' },
+    { prod: 'Iluminación' },
+    { prod: 'TV' },
+    { prod: 'Videojuegos' },
+    { prod: 'Otro' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
